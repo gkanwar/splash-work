a6e0feecf1a27dd2f5d10ec1fb1f12bd37852be5
GurtejKanwar <GurtejKanwar@f5c40389-3515-dcc7-9c99-1d7b04460124>
2011-11-16 23:35:32 +0000

    Snake works now!

---
+++ Snake/src/splash/game/snake/GameActivity.java
@@ -0,0 +1,15 @@
+package splash.game.snake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
