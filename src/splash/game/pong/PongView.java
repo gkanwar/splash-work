9fe474b7421b5051f4c5bf1c485dff523b93e239
GurtejKanwar <GurtejKanwar@f5c40389-3515-dcc7-9c99-1d7b04460124>
2011-11-19 23:17:32 +0000

    Finished the skeleton.

--- src/splash/game/pong/PongView.java
+++ src/splash/game/pong/PongView.java
@@ -13,21 +13,29 @@ import android.view.View;
 
 public class PongView extends View
 {
+	//Point that stores the screen size (use screenSize.x and screenSize.y to get width and height)
 	Point screenSize = new Point();
-	final int paddleVelMax = 6;
-	final int ballRad = 4;
-	final int paddleXDim = 10;
-	final int paddleYDim = 40;
-	int ballXStart;
-	int ballYStart;
-	int ballStartVelX = 2;
-	int ballStartVelY = 2;
-	int firstPaddleLoc = 0,secondPaddleLoc =0,ballXLoc = 50,ballYLoc = 50;
-	float ballXVel = 2,ballYVel = 2;
-	int lastCollision = 0;
-	int PlayerOneScore = 0;
-	int PlayerTwoScore = 0;
+	//Max velocity of the AI paddle
+	private static final int AI_MAX_VEL = 6;
+	//The ball's radius (in pixels)
+	private static final int BALL_RADIUS = 4;
+	//The paddle's dimensions
+	private static final int PADDLE_WIDTH = 10;
+	private static final int PADDLE_HEIGHT = 40;
+	//Variables used to set the starting conditions
+	private int ballXStart, ballYStart, ballStartVelX = 2, ballStartVelY = 2;
+	//Variables to keep track of the paddles and ball
+	private int firstPaddleLoc = 0, secondPaddleLoc = 0, ballXLoc = 50, ballYLoc = 50;
+	private float ballXVel = 2,ballYVel = 2;
 	
+	//Keep track of our last collision to make sure the ball doesn't collide twice in a row with the same object
+	private int lastCollision = 0;
+	
+	//Variables to track scores
+	private int playerOneScore = 0;
+	private int playerTwoScore = 0;
+	
+	//Our paint
 	private Paint mPaint = new Paint();
 	
 	
@@ -38,36 +46,18 @@ public class PongView extends View
     
 	protected void onDraw (Canvas canvas)
     {
-    	updatePositions();
-    	if (checkScore())
-    	{
-    		resetBall();
-    	}
+		//TODO: Update our position (see functions below)
+		//TODO: Update score and check for game over (see functions below)
     	
-    	//Draw the paddles and ball
-    	mPaint.setColor(Color.GREEN);
-    	canvas.drawRect(new Rect(0, paddleYDim + firstPaddleLoc, paddleXDim, firstPaddleLoc), mPaint);
-    	canvas.drawRect(new Rect(screenSize.x - paddleXDim, secondPaddleLoc, screenSize.x, secondPaddleLoc + paddleYDim),mPaint);
-    	canvas.drawCircle(ballXLoc, ballYLoc, ballRad, mPaint);
-    	canvas.drawText(""+PlayerOneScore, screenSize.x/4, screenSize.y/4, mPaint);
-    	canvas.drawText(""+PlayerTwoScore, 3*screenSize.x/4, screenSize.y/4, mPaint);
+    	//TODO: Draw the paddles, ball and scores
     	
+		//Invalidate so that it repeatedly draws, instead of just once
     	invalidate();
     }
 	
-	@Override
-	public void onSizeChanged(int width, int height, int oldw, int oldh)
-	{
-		screenSize.x = width;
-		screenSize.y = height;
-	}
+	//TODO: Override onSizeChanged so that screenSize.x is set to width and screenSize.y is set to height
 	
-	@Override
-    public boolean onTouchEvent(final MotionEvent event) 
-    {
-    	firstPaddleLoc = (int) event.getY();
-    	return true;
-    }
+	//TODO: Override onTouchEvent to set the player's paddle location to the y coordinate of the touch
 	
     private void resetBall()
     {
@@ -79,63 +69,39 @@ public class PongView extends View
     
     private void updatePositions()
     {
-    	//Pseudo-AI -> computer always moves towards the ball
-	    secondPaddleLoc += (ballYLoc > secondPaddleLoc + paddleYDim/2 ? Math.min( Math.abs(ballYLoc - secondPaddleLoc - paddleYDim/2), paddleVelMax ) 
-	    						: -Math.min( Math.abs(ballYLoc - secondPaddleLoc - paddleYDim/2), paddleVelMax ));
+    	//Pseudo-AI -> computer always moves towards the ball with limited velocity
+    	//Feel free to improve this :)
+	    secondPaddleLoc += (ballYLoc > secondPaddleLoc + PADDLE_HEIGHT/2 ? Math.min( Math.abs(ballYLoc - secondPaddleLoc - PADDLE_HEIGHT/2), AI_MAX_VEL ) 
+	    						: -Math.min( Math.abs(ballYLoc - secondPaddleLoc - PADDLE_HEIGHT/2), AI_MAX_VEL ));
     	
+    	//TODO: Apply velocities to the ball's position
+	    
+	    //TODO: Check collisions with paddles (see functions below) and change velocity based on it
+	    //Note: Make sure to check last collision to avoid colliding twice in a row
+	    //You could implement a system that increments velocity with every paddle collision
     	
-    	ballXLoc += ballXVel;
-    	ballYLoc += ballYVel;
+    	//TODO: Decrement lastCollision
     	
-    	if ((checkCollision(0) || checkCollision(1)) && lastCollision == 0)
-    	{
-    		ballXVel *= -(1.2);
-        	
-    		lastCollision = 20;
-    	}
-    	if (lastCollision > 0 )
-    	{
-    		lastCollision--;
-    	}
-    	
-    	if (ballYLoc > screenSize.y  || ballYLoc < 0 )
-    	{
-    		ballYVel*=-1;
-    	}
+    	//TODO: Check collisions with walls and change velocity based on it
     }
     
     private boolean checkCollision(int paddle) //Paddle should be 0 or 1
     {
     	if(paddle == 0) //Computer (higher x) paddle
     	{
-    		if(ballYLoc >= secondPaddleLoc && ballYLoc <= secondPaddleLoc + paddleYDim && ballXLoc + ballRad >= screenSize.x - paddleXDim)
-    		{
-    			return true;
-    		}
+    		//TODO: Check whether the ball is within the computer's paddle
     	}
     	else
     	{
-    		if(ballYLoc >= firstPaddleLoc && ballYLoc <= firstPaddleLoc + paddleYDim && ballXLoc - ballRad <= paddleXDim)
-    		{
-    			return true;
-    		}
+    		//TODO: Check whether the ball is within the player's paddle
     	}
 
     	return false;
     }
     
-    private boolean checkScore()
+    private boolean checkAndUpdateScore()
     {
-    	if (ballXLoc > screenSize.x)
-    	{
-    		PlayerOneScore++;
-    		return true;
-    	}
-    	else if (ballXLoc < 0)
-    	{
-    		PlayerTwoScore++;
-    		return true;
-    	}
+    	//TODO: Check whether the ball went off either edge and change scores
     	
     	return false;
     }
