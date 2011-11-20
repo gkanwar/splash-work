4fd3466c266f622a2142bf07c7a31d626c2e9f9b
androidevsplash@gmail.com <androidevsplash@gmail.com@f5c40389-3515-dcc7-9c99-1d7b04460124>
2011-11-20 16:43:58 +0000

    \0

--- branches/HeliSkeleton/src/splash/game/helicopter/GameActivity.java
+++ branches/HeliSkeleton/src/splash/game/helicopter/GameActivity.java
@@ -1,7 +1,10 @@
 package splash.game.helicopter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 
 public class GameActivity extends Activity
 {
